c8989f7044ea9442fdfc885fb78289edbbedd135
Sebastian Correa <scorrea@correo.um.edu.uy>
2021-05-04 13:14:17 -0300

    Make extension react to preferences changes in UI
    Add PreferencesEventListener to reset reset_to_default on extension run
    
    Add PreferencesUpdateListener to update preferences from UI values.
    
    Read default preferences by accessing manifest.json.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import json
 import logging
+from pathlib import Path
 from typing import List
 
 import requests
 from bs4 import BeautifulSoup
-from bs4.element import NavigableString, ResultSet
+from bs4.element import NavigableString
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent
+from ulauncher.api.shared.action.RenderResultListAction import \
+    RenderResultListAction
+from ulauncher.api.shared.event import (KeywordQueryEvent,
+                                        PreferencesEvent,
+                                        PreferencesUpdateEvent)
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 BASE_URL = "https://dle.rae.es"
@@ -16,6 +21,22 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 }
 CHARACTERS_PER_LINE = 80
+NUMERIC_PREFERENCES = {"max_suggested_items", "max_shown_definitions"}
+
+p = (
+    Path.home()
+    / ".local"
+    / "share"
+    / "ulauncher"
+    / "extensions"
+    / "rae"
+    / "manifest.json"
+)
+with p.open("r") as f:
+    DEFAULT_MANIFEST = json.load(f)
+DEFAULT_PREFERENCES = {
+    x["id"]: x["default_value"] for x in DEFAULT_MANIFEST["preferences"]
+}
 
 logger = logging.getLogger(__name__)
 
@@ -48,7 +69,15 @@ def chunkize_sentence(sentence: str, max_characters_per_chunk: int) -> List[str]
 class RAE(Extension):
     def __init__(self):
         super().__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(
+            KeywordQueryEvent, KeywordQueryEventListener()
+        )  # Handle user input via ulauncher.
+        self.subscribe(
+            PreferencesUpdateEvent, PreferencesUpdateListener()
+        )  # Handle preferences update via UI.
+        self.subscribe(
+            PreferencesEvent, PreferencesEventListener()
+        )  # To force reset toggle to be off by default.
 
     @staticmethod
     def handle_empty_word() -> List[ExtensionResultItem]:
@@ -75,7 +104,7 @@ class RAE(Extension):
 
             words = ""
             for child in definition.children:
-                if child.name not in ("span", "abbr"):
+                if child.name not in {"span", "abbr"}:
                     if isinstance(child, NavigableString):
                         words += child
                     else:
@@ -97,11 +126,12 @@ class RAE(Extension):
 
 
 class KeywordQueryEventListener(EventListener):
-    def on_event(self, event: KeywordQueryEvent, extension) -> RenderResultListAction:
+    def on_event(self, event: KeywordQueryEvent, extension: Extension) -> RenderResultListAction:
         items = []
 
         max_suggested_items = int(extension.preferences["max_suggested_items"])
         max_shown_definitions = int(extension.preferences["max_shown_definitions"])
+        print(f"{max_suggested_items=}, {max_shown_definitions=}")
 
         logger.info(f"max_suggested_items={max_suggested_items}")
         logger.info(f"max_shown_definitions={max_shown_definitions}")
@@ -133,6 +163,44 @@ class KeywordQueryEventListener(EventListener):
                 items = RAE.handle_multiple_defs(word, soup, max_shown_definitions)
 
         return RenderResultListAction(items)
+
+
+class PreferencesEventListener(EventListener):
+    def on_event(self, event: PreferencesEvent, extension: Extension):
+        """
+        Set this session's preferences to those of the last session, except for the reset toggle, which is reset to the default.
+        """
+        extension.preferences.update(event.preferences)
+        extension.preferences["reset_to_default"] = "-"
+        # FIXME: Doesn't set to '-'.
+
+
+class PreferencesUpdateListener(EventListener):
+    def on_event(self, event: PreferencesUpdateEvent, extension: Extension):
+        if event.id in NUMERIC_PREFERENCES:
+            if float(event.new_value).is_integer():
+                # Don't do the typical try/catch cause 4.2 (not "4.2") would be truncated to 4 and silently change the value to 4, instead of throwing an error.
+                extension.preferences[event.id] = int(event.new_value)
+                logger.info(
+                    f"{event.id} changed from {event.old_value} to {event.new_value}."
+                )
+            else:
+                extension.preferences[event.id] = int(event.old_value)
+                logger.info(
+                    f"{event.id} failed to change. Reverting to previous value of {event.old_value}. Given: {event.new_value}. Try an integer."
+                )
+        elif event.id == "reset_to_default" and event.new_value == "Reset":
+            for id_, val in DEFAULT_PREFERENCES.items():
+                old_val = extension.preferences[id_]
+                extension.preferences[id_] = val
+                logger.info(
+                    f"{id_} was reset from {old_val} to its default value of {val}."
+                )
+        else:
+            extension.preferences[event.id] = event.new_value
+            logger.info(
+                f"{event.id} changed from {event.old_value} to {event.new_value}."
+            )
 
 
 if __name__ == "__main__":
